7b8e4fb7c86c64ba844d5f3359011094f101bc6a
loganmelgoza <125330743+loganmelgoza@users.noreply.github.com>
2025-10-14 19:24:27 -0700

    lm - changed team to real team and added tests

--- src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/Developer.java
@@ -34,14 +34,13 @@ public class Developer {
      */
     
     public static Team getTeam() {
-        // TODO: Change this to your team name
-        Team team = new Team("f25-xx");
-        team.addMember("Alice");
-        team.addMember("Bob");
-        team.addMember("Chris G.");
-        team.addMember("Danny");
-        team.addMember("Eve");
-        team.addMember("Frances");
+        Team team = new Team("f25-14");
+        team.addMember("PRATIMA REDDY");
+        team.addMember("DANIEL JUNKYUK");
+        team.addMember("LOGAN CHRISTOPHER");
+        team.addMember("CATHERINE");
+        team.addMember("FERNANDO");
+        team.addMember("KEYI");
         return team;
     }
 }
